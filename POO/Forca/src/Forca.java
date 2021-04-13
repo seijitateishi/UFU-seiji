ed62be7aea9eee5f461e1aa610930e89f519bd15
Seiji Tateishi <seijitateishi@hotmail.com>
2021-04-13 18:34:30 -0300

    att

--- POO/Forca/src/Forca.java
+++ POO/Forca/src/Forca.java
@@ -1,30 +1,33 @@
-import jdk.nashorn.api.tree.ForLoopTree;
-
 public class Forca {
     String word = "palavra";
-    String usedLetters = "                          ";//26 espaços
-    String rightLetters[word.length]; //qtdd de espaços de word
-    for (int i = 0; i<word.length; i++){
-        this.rightletters[i] = "_";
+    int tam = word.length();
+    String[] guessedLetters = new String[tam];
+    String[] usedLetters = new String[26];
+    {
+    for (int i = 0; i < this.word.length(); i++){
+        this.guessedLetters[i] = "_";
     }
-    public String addLetter (char letter){
-        Int length = this.word.length();
-        for (int i = 0; i<length ; i++) {//verificar se a letra digitada esta na string
+    public void addLetter (char letter){
+        int index;
+        for (int i = 0; i<tam ; i++) {//verificar se a letra digitada esta na string
             if (letter == this.word.charAt(i)) 
-                this.rightLetters[i] = letter;//se estiver a letra vai entrar na mesma posição no rightletters
-            else 
-                this.usedLetters[i] = letter;//se nao estiver a letra vai entrar na mesma posição no wrongletters
+                this.guessedLetters[i] = letter;//se estiver a letra vai entrar na mesma posição no rightletters
+            else
+                index = usedLetters.indexOf(letter);
+                if (index != (-1))
+                    this.usedLetters[i] = letter;//se nao estiver a letra vai entrar na mesma posição no wrongletters  
         }
     }
     public void print (){
         System.out.println("letras usadas:"+usedLetters);
-        System.out.println("\npalavra:"+rightletters);
+        System.out.println("\npalavra:"+this.guessedletters);
     }
     public Boolean end (){
-        if (this.rightLetters.startsWith(this.word)){
-            true; 
-        }else {   
-            false;
-        }
+        if (this.guessedLetters == this.word)
+            return true; 
+        else   
+            return false;
+        
+    }
     }
 }
--- POO/Forca/src/main.java
+++ POO/Forca/src/main.java
@@ -3,17 +3,17 @@ import java.util.concurrent.ThreadLocalRandom;
 public class main {
     public static void public static void main(String[] args) {
         int forca = 1;
-        char chute;
-        Forca forca = new Forca();
-        while (forca){
+        Forca game = new Forca();
+        while (forca==1){
             System.out.println("digite uma letra:");
-            Scanner aux = new scanner(System.in);
+            Scanner aux = new Scanner(System.in);
             char letter = aux.next();
-            addLetter(letter);
-            print();
-            if end(){`
+            game.addLetter(letter);
+            game.print();
+            if game.end(){
                 System.out.println("fim de jogo!!!");
                 break;
-        }     
+            }     
+        }
     }
 }
