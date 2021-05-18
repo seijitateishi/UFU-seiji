84f899d2dc2d5ef266088ba4cb86a4803bbb8b6c
Seiji Tateishi <seijitateishi@hotmail.com>
2021-05-18 16:40:53 -0300

    Update trabalho2.hs

--- PF/trabalho/2/trabalho2.hs
+++ PF/trabalho/2/trabalho2.hs
@@ -14,4 +14,31 @@ x5=[11,12,13,14,15,5,4,3,2,1,16,17,18,19,20,10,9,8,7,6]
 x6=[1,12,3,14,5,15,4,13,2,11,6,17,8,19,20,10,9,18,7,16]
 x7=[20,8,2,11,13,3,7,18,14,4,16,10,15,1,9,17,19,12,5,6]
 --
-selecao
+bolha :: Ord a => [a] -> [a]
+bolha [] = []
+bolha lista = bolhaOrd lista (length lista)
+
+bolhaOrd :: Ord a => [a] -> Int -> [a]
+bolhaOrd lista 0 = lista
+bolhaOrd lista n = bolhaOrd (troca lista) (n-1)
+
+troca :: Ord a => [a] -> [a]
+troca [x] = [x]
+troca (x:y:zs)
+  |x > y = y : troca (x:zs)
+  |otherwise = x : troca (y:zs)
+--
+--variação 1
+bolha1 :: Ord a => [a] -> [a]
+bolha1 [] = []
+bolha1 lista = bolhaOrd1 lista (length lista)
+
+bolhaOrd1 :: Ord a => [a] -> Int -> [a]
+bolhaOrd1 lista 0 = lista
+bolhaOrd1 lista n = bolhaOrd1 (troca1 lista) (n-1)
+
+troca1 :: Ord a => [a] -> [a]
+troca1 [x] = [x]
+troca1 (x:y:zs)
+  |x > y = y : troca1 (x:zs)
+  |otherwise = x : troca1 (y:zs)
