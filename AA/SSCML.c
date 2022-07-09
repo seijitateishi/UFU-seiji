ef0810e4569bd2ae8de667d49cda50a502c3fa8b
seiji <seijitateishi@hotmail.com>
2022-07-09 14:16:34 -0300

    sscml aa

---
+++ AA/SSCML.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <array.h>
+
+int main(){
+    int VetorEntrada[], VetorFinal[], N;
+    for(int i = 0; i < N; i++){
+        printf("Digite o elemento %d: ", i);
+        scanf("%d", &VetorEntrada[i]);
+    }
+    N = lenght(VetorEntrada);
+    
+
+
+
+
+
+
+    for (int i = 0; i < N; i++)
+    {
+        printf("%d ,", VetorFinal[i]);
+    }
+    
+
+
+
+    return 0;
+}
