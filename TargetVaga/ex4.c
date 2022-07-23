4506956dcb31ec6da0caee3b20cef835dcd55fe3
seiji <seijitateishi@hotmail.com>
2022-07-23 13:50:26 -0300

    arrumando

--- TargetVaga/ex4.c
+++
@@ -1,8 +0,0 @@
-/*
-carro rp->franca 110km/h
-caminhao franca->rp 80km/h
-no momento em q se cruzarem vao 
-estar na mesma distancia de rp
-mas no instante seguinte o caminhao
-vai estar mais perto de rp
-*/
