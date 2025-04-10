0d23c493a79ea3fd7b5640190f63f05af1f6c75e
seiji <seijitateishi@hotmail.com>
2025-04-10 12:25:19 -0300

    arrumandopastappi

---
+++ PPI/Exemplos-Mysql/Exemplos-Mysql/conexaoMysql.php
@@ -0,0 +1,23 @@
+<?php
+
+function mysqlConnect()
+{
+  $db_host = "sql201.infinityfree.com";
+  $db_username = "if0_38295654";
+  $db_password = "ttUM8HXMRWa";
+  $db_name = "if0_38295654_ppi";
+
+  $options = [
+    PDO::ATTR_EMULATE_PREPARES => false, // desativa a execução emulada de prepared statements
+    PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION
+  ];
+
+  try {
+    $pdo = new PDO("mysql:host=$db_host;dbname=$db_name;charset=utf8mb4", $db_username, $db_password, $options);
+    return $pdo;
+  } 
+  catch (Exception $e) {
+    exit('Ocorreu uma falha na conexão com o MySQL: ' . $e->getMessage());
+  }
+}
+?>
